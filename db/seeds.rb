edf98d5f7f57191a0ce858b7796a9ffb939d073a
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-01-29 14:40:10 -0700

    relevant seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,18 @@ m4 = FactoryGirl.create(:section, name: "Module 4")
 
 puts " Successfully added sections from seed data. ".center(70, "~*~")
 
-FactoryGirl.create(:skill, name: "Pushing Logic Down the Stack", featured: true, section: m1)
-FactoryGirl.create(:skill, name: "Github Workflow", featured: true, section: m2)
-FactoryGirl.create(:skill, name: "Reduce/Inject Enumerable", featured: true, section: m4)
+FactoryGirl.create(:skill, name: "Pushing Logic Down the Stack", featured: true, section: m2)
+FactoryGirl.create(:skill, name: "Github Workflow", featured: true, section: m1)
+FactoryGirl.create(:skill, name: "Reduce/Inject Enumerable", featured: true, section: m1)
+FactoryGirl.create(:skill, name: "Rails Security", featured: true, section: m3)
 
-FactoryGirl.create(:skill, name: "Deployment", featured: false, section: m1)
-FactoryGirl.create(:skill, name: "TDD", featured: false, section: m3)
+FactoryGirl.create(:skill, name: "Deployment", featured: false, section: m2)
+FactoryGirl.create(:skill, name: "TDD", featured: false, section: m1)
 FactoryGirl.create(:skill, name: "Ruby Object Model", featured: false, section: m1)
-FactoryGirl.create(:skill, name: "Exposing An API", featured: false, section: m1)
-FactoryGirl.create(:skill, name: "Service Oriented Architechture", featured: false, section: m1)
+FactoryGirl.create(:skill, name: "Exposing An API", featured: false, section: m3)
+FactoryGirl.create(:skill, name: "Service Oriented Architechture", featured: false, section: m4)
+FactoryGirl.create(:skill, name: "How To Find A Job", featured: false, section: m4)
+FactoryGirl.create(:skill, name: "jQuery", featured: false, section: m2)
 
 puts " Successfully added skills from seed data. ".center(70, "~*~")
 
